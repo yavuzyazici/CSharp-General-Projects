b4b7d948983e74fee7ae890b70566dff3f5fccba
Yavuz Selim YAZICI <yavuzselimyazici41@gmail.com>
2023-12-14 20:33:11 +0300

    Drawing Triangle Project Uploaded

---
+++ Easy/DrawingTriangle/DrawingTriangle/Program.cs
@@ -0,0 +1,80 @@
+﻿int row;
+do
+{
+    Console.Write("Enter the number of rows of the triangle:");
+} while (!int.TryParse(Console.ReadLine(), out row) || row <= 1);
+
+Draw draw = new Draw();
+
+int input;
+do
+{
+    Console.WriteLine("Press 1 for full inside and 2 for empty inside");
+} while (!int.TryParse(Console.ReadLine(), out input) || (input != 1 && input != 2));
+
+switch (input)
+{
+    case 1:
+        draw.DrawTriangleFill(row);
+        break;
+    case 2:
+        draw.DrawTriangleUnfill(row);
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
+
+public class Draw
+{
+    public void DrawTriangleUnfill(int row)
+    {
+        int gaps = row;
+        int gapsin = 0;
+        int stars = 1;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < gaps; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j < stars; j++)
+            {
+                Console.Write("*");
+                for (int k = 1; k < gapsin; k++)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            stars = 2;
+            gapsin += 2;
+            gaps--;
+            if (row == i+2)
+            {
+                stars = row*2-1;
+                gapsin = 0;
+            }
+        }
+    }
+    public void DrawTriangleFill(int row)
+    {
+        int gaps = row;
+        int stars = 1;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < gaps; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k < stars; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            stars += 2;
+            gaps--;
+        }
+    }
+}
