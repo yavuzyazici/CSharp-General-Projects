ed4556402f9b8d502fcb6b99f9133ceb44b253e7
Yavuz Selim YAZICI <yavuzselimyazici41@gmail.com>
2024-01-02 17:50:08 +0300

    Algorithm Homework

---
+++ Easy/Algorithm Homework/Algorithm Homework/Algorithm Homework/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Homework
+{
+    public class Controller
+    {
+        public void Cutter(string text,int num)
+        {
+            char[] saaa = new char[text.Length];
+            List<char> letterSequence = new List<char>();
+
+
+            // Her bir harfi diziye ekle
+            for (int i = 0; i < text.Length; i++)
+            {
+                letterSequence.Add(text[i]);
+            }
+
+            letterSequence.RemoveAt(num);
+
+            Console.WriteLine(string.Join("", letterSequence));
+        }
+    }
+}
---
+++ Easy/Algorithm Homework/Algorithm Homework/Algorithm Homework/Program.cs
@@ -0,0 +1,40 @@
+﻿using Algorithm_Homework;
+
+Start:
+
+int number; string text;
+Console.WriteLine("Please enter a word and letter in the form 'word,letter' (Teststring,3)");
+string input = Console.ReadLine();
+
+string[] values = input.Split(",");
+
+if (values.Length > 2)
+{
+    Console.WriteLine("Wrong Input");
+    Thread.Sleep(700);
+    Console.Clear();
+    goto Start;
+}
+try
+{
+    number = Convert.ToInt32(values[1]);
+    text = values[0];
+}
+catch (Exception)
+{
+    Console.WriteLine("Wrong Input");
+    Thread.Sleep(700);
+    Console.Clear();
+    goto Start;
+}
+
+if (text.Length < number+1)
+{
+    Console.WriteLine(text);
+}
+else
+{
+    Controller controller = new Controller();
+
+    controller.Cutter(text, number);
+}
