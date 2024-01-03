0bf89b55d209ada66deeb1949abd65f54bf075f0
Yavuz Selim YAZICI <yavuzselimyazici41@gmail.com>
2024-01-03 16:18:45 +0300

    SentenceReverser App

---
+++ Easy/Sentence Reverse/Sentence Reverser/Sentence Reverser/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a sentence - Enter the sentence in the format specified in the example, Example: Hello I am Yavuz Selim");
+
+string sentence = Console.ReadLine();
+
+string[] words = sentence.Split(" ");
+string[] reversedWords = new string[words.Length];
+
+for (int i = 0; i < words.Length; i++)
+{
+    char[] letterSequence = words[i].ToCharArray();
+    Array.Reverse(letterSequence);
+    reversedWords[i] = new string(letterSequence);
+}
+
+Console.WriteLine("Inverted sentence:");
+Console.WriteLine(string.Join(" ", reversedWords));
