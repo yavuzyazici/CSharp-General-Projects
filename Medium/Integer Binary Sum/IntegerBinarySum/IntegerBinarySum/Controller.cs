b50e1952423495dbf00f2577453720fdf3f5dd5c
Yavuz Selim YAZICI <yavuzselimyazici41@gmail.com>
2024-01-07 15:15:20 +0300

    Integer Binary Sum Project Uploaded

---
+++ Medium/Integer Binary Sum/IntegerBinarySum/IntegerBinarySum/Controller.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerBinarySum
+{
+    public class Controller
+    {
+
+        public bool PcsPair(string input)
+        {
+            try
+            {
+                string[] strings = input.Split(" ");
+                if (strings.Count() % 2 == 1 || strings.Count() == 0)
+                {
+                    Clean();
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void BinarySum(string input)
+        {
+            string[] strings = input.Split(" ");
+            for (int i = 0; i < strings.Length - 1; i = i + 2)
+            {
+                if (strings[i] == strings[i + 1])
+                {
+                    int summed = Convert.ToInt32(strings[i]) + Convert.ToInt32(strings[i + 1]);
+                    int result = summed * summed;
+                    Console.Write(summed * summed + " ");
+                }
+                if (strings[i] != strings[i + 1])
+                {
+                    Console.Write(Convert.ToInt32(strings[i]) + Convert.ToInt32(strings[i + 1]) + " ");
+                }
+            }
+
+            Console.WriteLine("İşlem Tamamlandı.");
+        }
+
+        public void Clean()
+        {
+            Console.WriteLine("Wrong Input");
+            Thread.Sleep(500);
+            Console.Clear();
+        }
+    }
+}
---
+++ Medium/Integer Binary Sum/IntegerBinarySum/IntegerBinarySum/Program.cs
@@ -0,0 +1,17 @@
+﻿using IntegerBinarySum;
+
+//Project:Write a console application that takes the sum of n integer binary numbers entered from the screen, prints the sum if the numbers are different from each other, and prints the square of the sum if the numbers are the same.
+Start:
+Controller controller = new Controller();
+
+Console.WriteLine("Enter numbers with a space between them and an even number (Example: 2 3 1 5 2 5 3 3)");
+
+string input = Console.ReadLine();
+
+if (!controller.PcsPair(input))
+{
+    controller.Clean();
+    goto Start;
+}
+
+controller.BinarySum(input);
