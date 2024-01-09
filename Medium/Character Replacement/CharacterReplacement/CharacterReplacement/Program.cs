d8579b90c1ade3853b8f91d2d722e159b14e81b7
Yavuz Selim YAZICI <yavuzselimyazici41@gmail.com>
2024-01-09 16:32:00 +0300

    Character Replacement Project Uploaded

---
+++ Medium/Character Replacement/CharacterReplacement/CharacterReplacement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+Console.WriteLine("Enter a sentence - Enter the sentence in the format specified in the example, Example: Hello I am Yavuz Selim");
+
+string input = Console.ReadLine();
+
+string[] words = input.Split(" ");
+string[] wordsResults = new string[words.Length];
+
+for (int i = 0; i < words.Count(); i++)
+{
+    try
+    {
+        string word = words[i];
+
+        char fl = word.First();
+        char ll = word.Last();
+
+        if (word.Length == 1)
+        {
+            wordsResults[i] = word;
+            continue;
+        }
+
+        string swappedString = ll + word.Substring(1, word.Length - 2) + fl;
+
+        wordsResults[i] = swappedString;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Wrong Input");
+        return;
+    }
+    
+}
+
+Console.WriteLine("Sentence:");
+Console.WriteLine(string.Join(" ", wordsResults));
